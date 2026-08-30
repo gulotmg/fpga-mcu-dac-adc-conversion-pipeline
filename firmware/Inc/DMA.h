7ed064512fe227f75b142782086f4461667840ca
Marco Gulotta <290510294+gulotmg@users.noreply.github.com>
2026-08-30 20:46:56 +0200

    Delete firmware directory

--- firmware/Inc/DMA.h
+++
@@ -1,9 +0,0 @@
-#ifndef DMA_H_
-#define DMA_H_
-
-#include "stm32c031xx.h"
-
-void DMA_init(void);
-
-
-#endif /* DMA_H_ */
--- firmware/Inc/exti.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXTI_H_
-#define EXTI_H_
-
-#include "stm32c031xx.h"
-
-//functions
-void extiADC_init(void);
-
-#endif /* EXTI_H_ */
